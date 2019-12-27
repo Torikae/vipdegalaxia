2ef313e935fd5ea15bc17498efad9bce3fea826d
Torikae <kaedetomomizi@gmail.com>
2019-12-27 20:25:39 +0900

    test

--- run.py
+++ run.py
@@ -2,11 +2,14 @@
 # コメントアウトの方法は「ctrl+/」
 
 from flask import Flask
+from flask import render_template
+ 
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return '<html><body><h1>sample</h1></body></html>'
+    values = {"val1": 100, "val2" :200}
+    return render_template('index.html', values=values)
  
 if __name__ == '__main__':
     app.run()
